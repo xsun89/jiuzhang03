2b09ca83e52ab38aa2c0c40a13f5c41f23f77d1d
xsun89 <sunxin89@gmail.com>
2015-07-28 16:20:48 -0700

    add

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,23 @@ public class Main {
         a.right = new TreeNode(4);
 
         Solution solution = new Solution();
-        List<Integer> ret = solution.preorderTraversal(root);
+        List<Integer> ret = solution.preOrderStack(root);
         Iterator<Integer> it = ret.iterator();
         while(it.hasNext()){
             System.out.println(it.next());
+        }
+        System.out.println("---------------------------------------");
+        List<Integer> ret2 = solution.preOrderStack(root);
+        Iterator<Integer> it2 = ret.iterator();
+        while(it2.hasNext()){
+            System.out.println(it2.next());
+        }
+
+        System.out.println("---------------------------------------");
+        List<Integer> ret3 = solution.preorderTraversalDivideConquer(root);
+        Iterator<Integer> it3 = ret.iterator();
+        while(it3.hasNext()){
+            System.out.println(it3.next());
         }
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -22,7 +22,39 @@ public class Solution {
     }
 
     public List<Integer> preOrderStack(TreeNode root){
-        Stack<Integer> stack = new Stack<Integer>();
-        return null;
+        if(root == null)
+            return null;
+        List<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.empty()){
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if(node.right != null)
+                stack.push(node.right);
+            if(node.left != null){
+                stack.push(node.left);
+            }
+        }
+
+        return result;
+    }
+
+    public List<Integer> preorderTraversalDivideConquer(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        // null or leaf
+        if (root == null) {
+            return result;
+        }
+
+        // Divide
+        List<Integer> left = preorderTraversalDivideConquer(root.left);
+        List<Integer> right = preorderTraversalDivideConquer(root.right);
+
+        // Conquer
+        result.add(root.val);
+        result.addAll(left);
+        result.addAll(right);
+        return result;
     }
 }
